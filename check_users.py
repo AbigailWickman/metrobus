83d7aaaee858f15b422d5818c8ad3514029d5ba3
AbigailWickman <70304677+AbigailWickman@users.noreply.github.com>
2020-09-09 20:22:58 -0400

    Add files via upload

---
+++ check_users.py
@@ -0,0 +1,12 @@
+def check_users(current_users, new_users):
+    pass
+    lowerC_us = [l.lower() for l in current_users]
+    for new_users in new_us:
+        if new_users in lowerC_us:
+            print(f"Unfortunately, {new_users} has already been taken. Please choose another username.")
+        else:
+            print(f"Congratulations, {new_users} is available.")
+if __name__ == "__main__":
+    current_us = ['chris', 'haritha', 'sally', 'darnell', 'superman']
+    new_us = ['george', 'ringo', 'superman', 'hannibal']
+    check_users(current_us, new_us)
