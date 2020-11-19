bc77f5c017adbfd327c0d5bb74305e885251a348
AbigailWickman <70304677+AbigailWickman@users.noreply.github.com>
2020-11-18 22:37:25 -0500

    Add files via upload

--- Server/server.py
+++ Server/server.py
@@ -1,31 +1,11 @@
-from flask import Flask
-from markupsafe import escape
 
+from flask import Flask
 app = Flask(__name__)
 
+@app.route('/hello/<name>')
+def hello_name(name):
+    return {"hello": name}
+
 @app.route('/')
-def index():
-    return 'Index Page\n'
-
-@app.route('/hello')
-def hello():
-    key = "Hello"
-    name = "Abby"
-    return {key : name}
-
-@app.route('/dictionary')
-def dictionary():
-    return {"key": "value"}
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
+def hello_world():
+    return 'Hello, World!'
